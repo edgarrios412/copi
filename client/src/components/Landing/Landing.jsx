7a2d0b2ab740f7c407243ce9ec7b9b3150309037
edgarrios412 <edgarrios412@gmail.com>
2023-05-12 12:12:33 -0500

    Add routes

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,41 +1,11 @@
 import { useEffect, useState } from "react";
-import "./Landing.css"
-import Home from "../Home/Home"
+import style from "./Landing.module.css"
+import Nav from "../Nav/Nav"
 
 const Landing = () => {
-  const [step, setStep] = useState(1)
-  const [input, setInput] = useState()
-
-    const handleInput = (e) => {
-        const {name, value} = e.target;
-        setInput({
-            ...input,
-            [name]: value
-        })
-    }
-
   return(
     <>
-      {step == 1 && 
-      <div className="aparecer">
-        <h2 className="title">¿Donde te encuentras?</h2>
-        <div className="input-container">
-        <input className="input" name="start" onChange={handleInput} placeholder="Ciudad"/>
-        </div>
-        <h5 className="no-option" onClick={() => setStep(step+1)}>Prefiero no decirlo</h5>
-      </div>}
-      {step == 2 && 
-      <div className="aparecer">
-        <h2 className="title">¿A donde vas?</h2>
-        <div className="input-container">
-            <input className="input" name="destiny" onChange={handleInput} placeholder="Destino"/>
-        </div>
-        <h5 className="no-option" onClick={() => setStep(step+1)}>Prefiero no decirlo</h5>
-      </div>}
-      {step == 3 && 
-      <div className="aparecer">
-        <Home/>
-      </div>}
+      <Nav/>
     </>
   )
 };
