706593d66a187ecf04ffe747b8580f0bf73fbb91
edgarrios412 <edgarrios412@gmail.com>
2023-05-16 17:51:56 -0500

    Add nodemon command

--- api/app.js
+++ api/app.js
@@ -7,6 +7,10 @@ const app = express()
 app.use(morgan("dev"))
 app.use(cors())
 
+app.get("/test", (req,res) => {
+    res.send("Hola mundo")
+})
+
 app.listen(3000,() => {
     console.log("Server listening on port 3000")
 })
